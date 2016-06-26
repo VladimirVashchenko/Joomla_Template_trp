4b883cb40fc1b553ceae53d2c6d60c7717169a59
VladimirVashchenko <vashchenko.vs.88@gmail.com>
2016-06-26 21:06:28 +0300

    configuring the template

--- tpl/index.php
+++ tpl/index.php
@@ -1,7 +1,7 @@
-<?php defined("_JEXEC") or die();
-$doc = JFactory::getDocument();
-$doc->addStylesheet(JUri::base(TRUE) . "/templates/" . $doc->template . "css/styles.css");
-/*print_r($doc);*/
+<?php defined("_JEXEC") or die(); ?>
+<?php $doc = JFactory::getDocument();
+$doc->addStylesheet(JUri::base(TRUE) . '/templates/' . $doc->template . '/css/styles.css');
+//print_r($doc);
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
@@ -9,22 +9,73 @@ $doc->addStylesheet(JUri::base(TRUE) . "/templates/" . $doc->template . "css/sty
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
-    <jdoc::include type="head"/>
+    <jdoc:include type="head"/>
 </head>
 
 <body>
-<div class="body">
+<div class="main-div">
     <div class="header">
-        <a href="<?php echo JUri::base(TRUE) ?>"><img class="logo" width="342"  src="<?php echo JUri::base(TRUE)."/templates/".$doc->template;?>/images/logo_trp.svg"
+        <a class="logo-link" href="<?php echo $this->baseurl; ?>/"><img class="logo" src="<?php echo JUri::base(TRUE)."/templates/".$doc->template;?>/images/logo_trp.svg"
                                                       onerror="this.src='<?php echo JUri::base(TRUE)."/templates/".$doc->template;?>/images/logo_trp.png'" alt=""/></a>
+        <div class="right-menu">
+            <jdoc:include type="modules" name="search"/>
+            <div class="menu-div">
+                <jdoc:include type="modules" name="header"/>
+                <a class="fb-logo-link" href="#">
+                    <div class="fb-logo-div">
+                        <img class="fb-logo" src="<?php echo JUri::base(TRUE)."/templates/".$doc->template;?>/images/facebook-logo.svg"
+                             onerror="this.src='<?php echo JUri::base(TRUE)."/templates/".$doc->template;?>/images/facebook-logo.png'" alt=""/>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <jdoc:include type="component" />
 
-        <ul class="menu">
-            <li><a href="#">1</a></li>
-            <li><a href="#">1</a></li>
-            <li><a href="#">1</a></li>
-            <li><a href="#">1</a></li>
-        </ul>
-        <hr>
+        <footer class="footer">
+            <div class="footer-link">
+                <div class="footer-link-wrap">
+                    <a href="">
+                        <h1 class="copyr">TAX©</h1>
+                    </a>
+                </div>
+            </div>
+            <div class="footer-link">
+                <div class="footer-link-wrap">
+                    <a href="">
+                        <img class="footer-icon" src="<?php echo JUri::base(TRUE)."/templates/".$doc->template;?>/images/pointer.svg" onerror="this.src='<?php echo JUri::base(TRUE)."/templates/".$doc->template;?>/images/pointer.png'"
+                             alt=""/>
+                        <p class="phone">Asdasdasd</p>
+                    </a>
+                </div>
+            </div>
+            <div class="footer-link">
+                <div class="footer-link-wrap">
+                    <a href="">
+                        <img class="footer-icon" src="<?php echo JUri::base(TRUE)."/templates/".$doc->template;?>/images/phone.svg" onerror="this.src='<?php echo JUri::base(TRUE)."/templates/".$doc->template;?>/images/phone.png'" alt=""/>
+                        <p class="phone">Asdasdasd</p>
+                    </a>
+                </div>
+            </div>
+            <div class="footer-link">
+                <div class="footer-link-wrap">
+                    <a href="">
+                        <img class="footer-icon" src="<?php echo JUri::base(TRUE)."/templates/".$doc->template;?>/images/letter.svg" onerror="this.src='<?php echo JUri::base(TRUE)."/templates/".$doc->template;?>/images/letter.png'" alt=""/>
+                        <p class="phone">Asdasdasd</p>
+                    </a>
+                </div>
+            </div>
+            <div class="footer-link">
+                <div class="footer-link-wrap">
+                    <a href="">
+                        <img class="footer-icon" src="<?php echo JUri::base(TRUE)."/templates/".$doc->template;?>/images/facebook-logo-round.svg"
+                             onerror="this.src='<?php echo JUri::base(TRUE)."/templates/".$doc->template;?>/images/facebook-logo-round.png'" alt=""/>
+                        <p class="phone">Asdasdasd</p>
+                    </a>
+                </div>
+            </div>
+        </footer>
     </div>
 </div>
 </body>
